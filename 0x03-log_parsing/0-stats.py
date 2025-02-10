f882b1c3480fa92f33a6343d4d088360d56c7195
Ahmed Wael <Xahmedwaheed@gmail.com>
2025-02-10 17:12:12 +0000

    log status

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""reading and processing a stream of data"""
+import sys
+import re
+
+
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                '404': 0, '405': 0, '500': 0}
+
+total_size = 0
+count = 0
+
+try:
+    for line in sys.stdin:
+        lines_l = line.split(" ")
+        if len(lines_list) > 5:
+            st_code = lines_l[-2]
+            size = int(lines_l[-1])
+
+            if st_code in status_codes.keys():
+                status_codes[st_code] += 1
+
+            total_size += size
+            count += 1
+
+            if count == 10:
+                count = 0
+
+                print('File size: {}'.format(total_size))
+
+                for key, val in sorted(status_codes.items()):
+                    if val != 0:
+                        print('{}: {}'.format(key, val))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, val in sorted(status_codes.items()):
+        if val != 0:
+            print('{}: {}'.format(key, val))
